2eeddc52f2d5dbf96ee058810b18fd8ce1d23fea
rozsnono <rozsnono@gmail.com>
2022-04-12 16:01:53 +0200

    ++++

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import * as express from "express";
 import * as mongoose from "mongoose";
 import IController from "./interfaces/controller.interface";
-import loggerMiddleware from "./middleware/logger.middleware";
 import onesideModel from "./controllers/oneside.model";
 
 export default class App {
@@ -10,15 +9,18 @@ export default class App {
     constructor(controllers: IController[]) {
         this.app = express();
         this.connectToTheDatabase();
-        this.app.use(express.json());
-        controllers.forEach(controller => {
-            this.app.use("/", controller.router);
-        });
+        this.initializeControllers(controllers);
     }
 
     public listen(): void {
         this.app.listen(5000, () => {
             console.log(`App listening on the port 5000`);
+        });
+    }
+
+    private initializeControllers(controllers: IController[]) {
+        controllers.forEach(controller => {
+            this.app.use("/", controller.router);
         });
     }
 
