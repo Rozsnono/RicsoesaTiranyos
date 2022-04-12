4c70360f0817f33c6517b9d66c16e1d01b8dbc7f
rozsnono <rozsnono@gmail.com>
2022-04-12 08:13:06 +0200

    ++

---
+++ src/controllers/machine.model.ts
@@ -0,0 +1,22 @@
+// https://mongoosejs.com/docs/validation.html#built-in-validators
+
+import { Schema, model } from "mongoose";
+
+const machineSchema = new Schema(
+    {
+        _id: String,
+        type: {
+            type: String,
+            required: true,
+        },
+        specs: {
+            type: Object,
+            required: true,
+        },
+    },
+    { versionKey: false },
+);
+
+const onesideModel = model("machines", machineSchema, "Machines");
+
+export default onesideModel;
--- src/controllers/nside.controller.ts
+++ src/controllers/nside.controller.ts
@@ -2,26 +2,54 @@ import { Request, Response, Router } from "express";
 import Controller from "../interfaces/controller.interface";
 import nsideModel from "./nside.model";
 import onesideModel from "./oneside.model";
+import linkModel from "./link.model";
+import machineModel from "./machine.model";
 
 export default class nsideController implements Controller {
     public path = "/api/xyz";
     public router = Router();
     private nsideM = nsideModel;
     private onesideM = onesideModel;
+    private linkM = linkModel;
+    private machineM = machineModel;
 
     constructor() {
         this.router.get("/api/dates", this.getAll);
+        this.router.get("/api/links", this.getAllLink);
+        this.router.get("/api/machines", this.getAllMachine);
         this.router.get("/api/dates/:id", this.getById);
+
         this.router.post("/api/date", this.create);
         this.router.post("/api/game", this.createGame);
-        this.router.patch("/api/xyz/:id", this.modifyPATCH);
-        this.router.put("/api/xyz/:id", this.modifyPUT);
+        this.router.post("/api/link", this.createLink);
+        this.router.post("/api/machine", this.createMachine);
+
+        this.router.put("/api/date/:id", this.modifyPUTdate);
+        this.router.put("/api/link/:id", this.modifyPUTlink);
+        this.router.put("/api/machine/:id", this.modifyPUTmachine);
+
         this.router.delete("/api/dates/:id", this.delete);
     }
 
     private getAll = async (req: Request, res: Response) => {
         try {
             const data = await this.nsideM.find().populate("game", "-_id");
+            res.send(data);
+        } catch (error) {
+            res.status(400).send(error.message);
+        }
+    };
+    private getAllLink = async (req: Request, res: Response) => {
+        try {
+            const data = await this.linkM.find();
+            res.send(data);
+        } catch (error) {
+            res.status(400).send(error.message);
+        }
+    };
+    private getAllMachine = async (req: Request, res: Response) => {
+        try {
+            const data = await this.machineM.find();
             res.send(data);
         } catch (error) {
             res.status(400).send(error.message);
@@ -68,6 +96,32 @@ export default class nsideController implements Controller {
         }
     };
 
+    private createLink = async (req: Request, res: Response) => {
+        try {
+            const body = req.body;
+            const createdDocument = new this.linkM({
+                ...body,
+            });
+            const savedDocument = await createdDocument.save();
+            res.send(savedDocument);
+        } catch (error) {
+            res.status(400).send(error.message);
+        }
+    };
+
+    private createMachine = async (req: Request, res: Response) => {
+        try {
+            const body = req.body;
+            const createdDocument = new this.machineM({
+                ...body,
+            });
+            const savedDocument = await createdDocument.save();
+            res.send(savedDocument);
+        } catch (error) {
+            res.status(400).send(error.message);
+        }
+    };
+
     private modifyPATCH = async (req: Request, res: Response) => {
         try {
             const id = req.params.id;
@@ -83,13 +137,43 @@ export default class nsideController implements Controller {
         }
     };
 
-    private modifyPUT = async (req: Request, res: Response) => {
+    private modifyPUTdate = async (req: Request, res: Response) => {
         try {
             const id = req.params.id;
             const body = req.body;
             const modificationResult = await this.nsideM.replaceOne({ _id: id }, body, { runValidators: true });
             if (modificationResult.modifiedCount) {
-                const updatedDoc = await this.nsideM.findById(id).populate("FK_neve", "-_id");
+                const updatedDoc = await this.nsideM.findById(id).populate("game", "-_id");
+                res.send(updatedDoc);
+            } else {
+                res.status(404).send(`Document with id ${id} not found!`);
+            }
+        } catch (error) {
+            res.status(400).send(error.message);
+        }
+    };
+    private modifyPUTlink = async (req: Request, res: Response) => {
+        try {
+            const id = req.params.id;
+            const body = req.body;
+            const modificationResult = await this.linkM.replaceOne({ _id: id }, body, { runValidators: true });
+            if (modificationResult.modifiedCount) {
+                const updatedDoc = await this.linkM.findById(id);
+                res.send(updatedDoc);
+            } else {
+                res.status(404).send(`Document with id ${id} not found!`);
+            }
+        } catch (error) {
+            res.status(400).send(error.message);
+        }
+    };
+    private modifyPUTmachine = async (req: Request, res: Response) => {
+        try {
+            const id = req.params.id;
+            const body = req.body;
+            const modificationResult = await this.machineM.replaceOne({ _id: id }, body, { runValidators: true });
+            if (modificationResult.modifiedCount) {
+                const updatedDoc = await this.machineM.findById(id).populate;
                 res.send(updatedDoc);
             } else {
                 res.status(404).send(`Document with id ${id} not found!`);
