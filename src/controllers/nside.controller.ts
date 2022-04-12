99017f0886ddb89d185914bff7f0e767a61f8aca
rozsnono <rozsnono@gmail.com>
2022-04-12 15:52:47 +0200

    ++++

--- src/controllers/nside.controller.ts
+++ src/controllers/nside.controller.ts
@@ -14,7 +14,6 @@ export default class nsideController implements Controller {
     private machineM = machineModel;
 
     constructor() {
-        this.router.get("",this.getAll)
         this.router.get("/api/dates", this.getAll);
         this.router.get("/api/links", this.getAllLink);
 		this.router.get("/api/links/:id", this.getLinkById);
