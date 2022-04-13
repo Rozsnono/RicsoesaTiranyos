477ad7c5ecdf0c8b732b728a45236c5b19b704e9
rozsnono <rozsnono@gmail.com>
2022-04-13 16:10:12 +0200

    +++

--- RicsoesaTiranyos/src/app/app.module.ts
+++ RicsoesaTiranyos/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { AppComponent } from './app.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { HomeComponent } from './home/home.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MachinesComponent } from './machines/machines.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import {MatTableModule} from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 @NgModule({
@@ -28,7 +34,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     MatTabsModule,
     MatTableModule,
-    
+    MatSelectModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory,
--- RicsoesaTiranyos/src/app/home/home.component.ts
+++ RicsoesaTiranyos/src/app/home/home.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CalendarEvent, CalendarView } from 'angular-calendar';
-import { startOfDay, startOfHour } from 'date-fns';
 import { HttpClient } from '@angular/common/http';
+import {FormControl, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
+
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { id } from 'date-fns/locale';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +18,11 @@ import { Router } from '@angular/router';
 
 export class HomeComponent implements OnInit {
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, @Inject(MAT_DATE_LOCALE) private _locale: string) { }
 
-  async ngOnInit() {
-    await this.getDates();
+
+  ngOnInit() {
+    this.getDates();
     this.getLinkById();
     this.getGame();
   }
@@ -26,7 +32,6 @@ export class HomeComponent implements OnInit {
   viewDate: Date = new Date();
   view: CalendarView = CalendarView.Month;
   CalendarView = CalendarView;
-  games: any[] = [];
   colors: any = {
     red: {
       primary: '#ad2121',
@@ -41,14 +46,136 @@ export class HomeComponent implements OnInit {
       secondary: '#000'
     }
   }
-
+  
+  games: any[] = [];
   events: CalendarEvent[] = [];
 
+  time: any;
+
+  newDateDate: any;
+  newDateGameId: any;
+  newDateId: Number = 0;
+  NewGameName: any;
+  NewGameColor: any;
+
+  errorMessage: any;
+
+  base64textString: any;
+
+  onUploadChange(evt: any) {
+    const file = evt.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = this.handleReaderLoaded.bind(this);
+      reader.readAsBinaryString(file);
+    }
+  }
+
+  handleReaderLoaded(e: any) {
+    this.base64textString = btoa(e.target.result);
+    console.log(this.base64textString);
+  }
+
+  gameModel: any = {};
+  gameId: any;
+
+  
+  model = {
+    _id: Number,
+    date: String,
+    game: Number
+  }
+
+  getGameIdforSelect(e: any){
+    this.model.game = e;
+  }
+
+  async newDateAdd(){
+    let d = new Date(this.newDateDate);
+    this.newDateDate = d.getFullYear() + "-" + ((d.getMonth()+1) < 10 ? '0' +(d.getMonth()+1) : (d.getMonth()+1)) + "-" + ((d.getDate()) < 10 ? '0' +(d.getDate()) : (d.getDate())) + "T" + this.time.toString();
+    console.log(this.newDateDate);
+
+    console.log(this.newDateGameId);
+
+    
+    if(this.newDateGameId == 99){
+      await this.newGameAdd();
+    }else{
+      await this.getGameId(this.newDateGameId);
+    }
+
+    console.log(this.events);
+    let tmp :any = this.events[this.events.length-1].id;
+    let tmpNumber: any = parseInt(tmp)+1;
+    this.model._id = tmpNumber;
+    this.model.date = this.newDateDate.toString();
+
+    const newModel = {
+      _id: Number,
+      game: Number,
+      date: String
+    }
+
+    newModel._id = this.model._id;
+    newModel.game = this.model.game;
+    newModel.date = this.model.date;
+    
+
+    this.http.post(this.backendURL + "/api/date",newModel).subscribe({
+      next: (data: any) => {window.location.reload();},
+      error: error => {this.errorMessage = true; console.log(error.message);}
+    })
+    
+  }
+  gameAdded: any;
+  newGameAdd(){
+    const newID = this.games[this.games.length-1]._id+1;
+
+    const model = {
+      _id: Number,
+      name: String,
+      color: String,
+      picture: String,
+    }
+
+    model._id = newID;
+    model.name = this.NewGameName;
+    model.color = this.NewGameColor ? this.NewGameColor : "#000";
+    model.picture = this.base64textString;
+
+    
+    this.http.post(this.backendURL + "/api/game",model).subscribe({
+        next: (data: any) => {this.gameAdded = true;},
+        error: error => {this.errorMessage = true; console.log(error.message)}
+    })
+
+    this.gameId = newID;
+  }
+
+  deleteDate(){
+    this.http.delete(this.backendURL + "/api/dates/" + this.deleteId)
+        .subscribe({
+            next: data => {
+              window.location.reload();
+            },
+            error: error => {
+                console.error('There was an error!', error);
+            }
+        });
+  }
+
   selectedEvent: any;
+  newEvent: any;
 
   tmpEvent: any;
   tmpEvents: any[] = [];
   tmpDate: any;
+
+  selectedGame: any;
+  selectControl = new FormControl('', Validators.required);
+  selectFormControl = new FormControl('', Validators.required);
 
   async getDates(){
     await this.http.get<any[]>(this.backendURL+"/api/dates").subscribe(
@@ -84,23 +211,29 @@ export class HomeComponent implements OnInit {
     return date[0].replaceAll('-','. ') + " " + date[1].split(':')[0] + ":" + date[1].split(':')[2].split('.')[0];
   }
 
+  deleteId: any;
+
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     this.selectedEvent = null;
-    this.getDateId(events[0].id);
-    this.tmpDate = date.getFullYear() + ". " + (date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth()) + ". " + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ". " +(date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
-    this.selectedEvent.date = this.tmpDate;
-    
-    this.selectedEvent.selected = true;
-    
-
+    this.newDateDate = date;
+    if(events.length != 0){
+      this.getDateId(events[0].id);
+      this.deleteId = events[0].id;
+      this.tmpDate = date.getFullYear() + ". " + (date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth()) + ". " + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ". " +(date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+      this.selectedEvent.date = this.tmpDate;
+      
+      this.selectedEvent.selected = true;
+    }else{
+      this.newEvent = true;
+    }
     //this.openAppointmentList(date)
   }
 
   getGame(){
     this.http.get<any[]>(this.backendURL+"/api/games").subscribe(
       {
-        next: (data: any) => this.games = data,
+        next: (data: any) => {this.games = data;console.log(this.games);},
         error: error => console.log(error)
       }
     )
@@ -118,8 +251,7 @@ export class HomeComponent implements OnInit {
   getGameId(id: any){
     this.http.get<any[]>(this.backendURL+"/api/games/"+id).subscribe(
       {
-        next: (data: any) => {this.selectedEvent.name = data.name;
-          this.selectedEvent.picture = data.picture;},
+        next: (data: any) => {console.log(data); this.gameModel = data; this.model.game = data._id},
         error: error => console.log(error)
       }
     )
