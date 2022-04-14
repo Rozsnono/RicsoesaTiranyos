07e4c501660ea0ea6faadb9d85c8b93429bde58c
rozsnono <rozsnono@gmail.com>
2022-04-14 20:44:31 +0200

    ++++

--- src/controllers/youtubeLink.model.ts
+++ src/controllers/youtubeLink.model.ts
@@ -2,17 +2,33 @@
 
 import { Schema, model } from "mongoose";
 
-const linkSchema = new Schema(
+const youtubeSchema = new Schema(
     {
         _id: Number,
         link: {
             type: String,
             required: true,
         },
+        picture: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        date: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: Array,
+            required: false
+        }
     },
     { versionKey: false },
 );
 
-const onesideModel = model("links", linkSchema, "Links");
+const onesideModel = model("youtube", youtubeSchema, "Youtube");
 
 export default onesideModel;
