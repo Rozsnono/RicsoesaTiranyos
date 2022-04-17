cf4dc975dd63da78a01c794645de380c0df41e1b
rozsnono <rozsnono@gmail.com>
2022-04-17 10:00:54 +0200

    +++

--- RicsoesaTiranyos/src/app/app.component.ts
+++ RicsoesaTiranyos/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   backendURL = "https://ricsoesatiranyos.herokuapp.com";
   links: Array<any> = [];
+  isLoaded: any = false;
 
   constructor(private http: HttpClient){}
 
@@ -21,7 +22,7 @@ export class AppComponent {
   getLinks(){
     this.http.get<any[]>(this.backendURL+"/api/links").subscribe(
       {
-        next: (data: any) => {this.links = data;},
+        next: (data: any) => {this.links = data; this.isLoaded = true; console.log("asd");},
         error: error => console.log(error)
       }
     )
