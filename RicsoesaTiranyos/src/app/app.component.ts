24431d431d4bb2ab5fed300173e77ac7e83b9960
rozsnono <rozsnono@gmail.com>
2022-04-17 10:06:02 +0200

    ++++

--- RicsoesaTiranyos/src/app/app.component.ts
+++ RicsoesaTiranyos/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RouteConfigLoadEnd, RouteConfigLoadStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +12,25 @@ export class AppComponent {
 
   backendURL = "https://ricsoesatiranyos.herokuapp.com";
   links: Array<any> = [];
-  isLoaded: any = false;
+  loadingRouteConfig: boolean;
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private router: Router){}
 
   ngOnInit(){
+    this.router.events.subscribe(event => {
+      if (event instanceof RouteConfigLoadStart) {
+          this.loadingRouteConfig = true;
+      } else if (event instanceof RouteConfigLoadEnd) {
+          this.loadingRouteConfig = false;
+      }
+  });
     this.getLinks();
   }
 
   getLinks(){
     this.http.get<any[]>(this.backendURL+"/api/links").subscribe(
       {
-        next: (data: any) => {this.links = data; this.isLoaded = true; console.log("asd");},
+        next: (data: any) => {this.links = data;},
         error: error => console.log(error)
       }
     )
