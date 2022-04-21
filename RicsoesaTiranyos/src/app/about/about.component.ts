5472294564d8bf9bb642d1d25001cd731aa083e9
rozsnono <rozsnono@gmail.com>
2022-04-21 22:17:31 +0200

    ++++++

--- RicsoesaTiranyos/src/app/about/about.component.ts
+++ RicsoesaTiranyos/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
@@ -6,12 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  safeSrc: SafeResourceUrl;
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-
+    this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl("https://player.twitch.tv/?channel=ricsoesatiranyos&parent=" + this.getUrl());
     
+  }
+
+  getUrl(){
+    return window.location.href.split('//')[1].split('/')[0];
   }
 
 }
