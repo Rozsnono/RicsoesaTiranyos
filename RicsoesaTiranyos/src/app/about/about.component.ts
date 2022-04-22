15feb67e8749dfee4d4576f9fe86c266ace5ebbb
rozsnono <rozsnono@gmail.com>
2022-04-22 16:06:47 +0200

    aboutPage

--- RicsoesaTiranyos/src/app/about/about.component.ts
+++ RicsoesaTiranyos/src/app/about/about.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
@@ -8,16 +9,80 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class AboutComponent implements OnInit {
   safeSrc: SafeResourceUrl;
+  backendURL = "https://ricsoesatiranyos2.herokuapp.com";
 
-  constructor(private sanitizer: DomSanitizer) { }
+  youtubeURL: SafeResourceUrl;
+  twitchURL: SafeResourceUrl;
+  instagramURL: SafeResourceUrl;
+
+  youtube: String;
+  twitch: String;
+  instagram: String;
+
+
+  links: any[] = [];
+
+  constructor(private sanitizer: DomSanitizer, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getSubs();
+
     this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl("https://player.twitch.tv/?channel=ricsoesatiranyos&parent=" + this.getUrl());
-    
   }
 
   getUrl(){
+    if(window.location.href.includes("localhost")){
+      return "localhost";
+    }
     return window.location.href.split('//')[1].split('/')[0];
+  }
+
+  getSubs(){
+    this.http.get<any[]>(this.backendURL+"/api/links").subscribe(
+      {
+        next: (data: any) => 
+          {
+            this.links = data; this.youtubeURL = this.getLinkByName("youtube"); this.twitchURL = this.getLinkByName("twitch"); this.instagramURL = this.getLinkByName("instagram");
+            this.youtube = this.subConverter("youtube",data);
+            this.twitch = this.subConverter("twitch",data);
+            this.instagram = this.subConverter("instagram",data);
+          },
+        error: error => console.log(error)
+      }
+    )
+  }
+
+  subConverter(type: any, data: any[]): String{
+    const tmp:any = data.filter(x => x.name === type);
+    const subs = tmp[0].subs.toString();
+
+    if(parseInt(subs) > 100000){
+      const sub = subs.slice();
+      return sub[0]+sub[1]+sub[2] + "k+";
+    }
+
+    if(parseInt(subs) > 1000){
+      const sub = subs;
+      let subIn = "";
+      let finalSubs = "";
+      for (let index = sub.length-1; index >= 0; index--) {
+        subIn += ((sub.length-1 - index) % 3 == 0 ? " ": "") + sub[index];
+      }
+
+      for (let index = subIn.length-1; index >= 0; index--) {
+        finalSubs += subIn[index];
+      }
+      
+      return finalSubs;
+    }
+
+
+    return subs;
+  }
+
+  getLinkByName(type: any):any{
+    const tmp: any = this.links.filter(x => x.name === type)[0];
+    return tmp.link;
   }
 
 }
--- RicsoesaTiranyos/src/app/app.component.ts
+++ RicsoesaTiranyos/src/app/app.component.ts
@@ -14,6 +14,13 @@ export class AppComponent {
   links: Array<any> = [];
   loading: boolean;
 
+
+
+  face: any;
+  twitch: any;
+  youtube: any;
+  insta: any;
+
   constructor(private http: HttpClient, private router: Router){
     router.events.subscribe(event => {
       if(event instanceof NavigationStart) {
@@ -31,7 +38,12 @@ export class AppComponent {
   getLinks(){
     this.http.get<any[]>(this.backendURL+"/api/links").subscribe(
       {
-        next: (data: any) => {this.links = data;},
+        next: (data: any) => {this.links = data;
+          this.face = this.links.filter(x => x.name === "facebook")[0].link;
+          this.youtube = this.links.filter(x => x.name === "youtube")[0].link;
+          this.twitch = this.links.filter(x => x.name === "twitch")[0].link;
+          this.insta = this.links.filter(x => x.name === "instagram")[0].link;
+        },
         error: error => console.log(error)
       }
     )
