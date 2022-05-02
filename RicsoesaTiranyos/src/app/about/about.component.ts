2bb04b059147636baa4592bd2add065770a8c24b
rozsnono <rozsnono@gmail.com>
2022-05-02 19:21:04 +0200

    about page

--- RicsoesaTiranyos/src/app/about/about.component.ts
+++ RicsoesaTiranyos/src/app/about/about.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 
@@ -7,11 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  backendURL = "https://ricsoesatiranyos2.herokuapp.com";
 
   ngOnInit(): void {
-
+    this.getLinks();
   }
 
+  links: any[] = [];
+
+  getLinks(){
+    this.http.get<any[]>(this.backendURL+"/api/links").subscribe(
+      {
+        next: (data: any) => {this.links = data;},
+        error: error => console.log(error)
+      }
+    )
+  }
+
+  getLinkByName(name: any){
+    return this.links.filter(x => x.name === name)[0].link;
+  }
   
 }
